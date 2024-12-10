5325ae5374d4e99f701ec7db9abd818b56692d14
Victor Camon Martinez <46001842+v-camon@users.noreply.github.com>
2024-12-10 23:53:20 +0100

    V 0.1
    Added background, fixed model position, file structure change

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const modelViewer = document.querySelector('model-viewer');
 
-const maxRotation = 360;
-const inital_position = -50;
+const maxRotation = 300;
+const inital_position = -40;
 
 window.addEventListener('scroll', () => {
     const scrollPercent = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
     const rotation_scroll = scrollPercent * maxRotation;
+    const rotation = inital_position + rotation_scroll;
     
-    modelViewer.orientation = `0deg 0deg ${rotation_scroll}deg`;
+    modelViewer.orientation = `0deg 0deg ${rotation}deg`;
 });
 
 modelViewer.orientation = `0deg 0deg ${inital_position}deg`;
