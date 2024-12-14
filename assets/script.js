e3f3a94b44800ad68f970f931189316c4860796a
v-camon <vicama018@gmail.com>
2024-12-14 17:52:50 +0100

    Info add to heroes.html

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,31 @@ window.addEventListener('scroll', () => {
 });
 
 modelViewer.orientation = `0deg 0deg ${inital_position}deg`;
+
+
+
+document.querySelector('[alt="Trailer"]').addEventListener('click', function() {
+    const iframe = document.querySelector('iframe');
+    const close_button = document.querySelector('.close_button');
+
+    iframe.classList.add('active');
+    close_button.classList.add('active');
+    
+    let currentSrc = iframe.src;
+    iframe.src = currentSrc + '?autoplay=1';
+});
+
+
+const closeButton = document.querySelector('.close_button');
+closeButton.addEventListener('click', toggleIframe);
+
+function toggleIframe() {
+    const iframe = document.querySelector('iframe');
+    const closeButton = document.querySelector('.close_button');
+    
+    iframe.classList.toggle('active');
+    closeButton.classList.toggle('active');
+    
+    // Reset iframe src to stop video playback
+    iframe.src = iframe.src.split('?')[0];
+}
